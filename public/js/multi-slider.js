307ab7c6e75bbdf0d5762616ee0409c6dd68b526
Cazs <casperndlovu42@gmail.com>
2017-09-11 14:18:13 +0200

    Added carousel for affiliate/partner companies on home screen.

--- public/js/multi-slider.js
+++ public/js/multi-slider.js
@@ -1,18 +1,23 @@
-$(document).ready(function(){
-    $('.carousel[data-type="multi"] .item').each(function(){
-      var next = $(this).next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-      
-      for (var i=0;i<4;i++) {
-        next=next.next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-          }
-        
-        next.children(':first-child').clone().appendTo($(this));
-      }
-    });
-    });
+$('#partners_carousel').carousel(
+{
+	interval: 2500
+});
+
+$('.carousel .partner-item').each(function()
+{
+  var next = $(this).next();
+  if (!next.length)
+  {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0)
+  {
+    next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');  
+  }
+  else
+  {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));  
+  }
+});
