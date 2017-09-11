86a5f9c2c4fac3ecd36498f939487cc279ccee71
Cazs <casperndlovu42@gmail.com>
2017-09-11 12:13:45 +0200

    Website at beta stage. All basic pages have been created with the correct information.

---
+++ routes/views/gallery.js
@@ -0,0 +1,31 @@
+var keystone = require('keystone');
+var fs = require('fs');
+
+exports = module.exports = function (req, res)
+{
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Set locals
+	locals.section = 'gallery';
+
+	// Load the galleries by sortOrder
+	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
+
+	fs.readdir(__dirname + '/../../public/images/gallery', function(err, dir_data)
+	{
+		console.log('reading gallery images.\n');
+		if(err)
+		{
+			console.log(err);
+			res.status(500).send(err);
+			// Render the view
+			view.render('gallery');
+			return;
+		}
+		//view.query('imgs', dir_data);
+		// Render the view
+		view.render('gallery', {"images": dir_data});
+	});
+
+};
